328f522c406ba595cbe9c3c4679322ef774c3be0
LENGLET <stephane.lenglet@gmail.com>
2019-03-10 23:19:58 +0100

    TestExercice1

--- src/test/TestExercice1.scala
+++ src/test/TestExercice1.scala
@@ -1,5 +1,20 @@
 package test
 
-object TestExercice1 {
+import com.holdenkarau.spark.testing.{RDDComparisons, SharedSparkContext}
+import org.scalatest.FunSuite
+import fr.lenglet.sparktoolbox.exercices.Exercice1.rawPersonne
+
+
+class TestExercice1 extends FunSuite with SharedSparkContext with RDDComparisons {
+
+  test("Test Personne") {
+
+    val list = List("1,\"Dupont\",\"Stef\",44","2,\"Dupont\",\"Raph\",41")
+    val rddList = sc.parallelize(list)
+    val resultRDD  = rawPersonne(rddList)
+    val result = resultRDD.map(x => x.age).reduce(_+_)
+
+    assert(result === 85 )
+  }
 
 }

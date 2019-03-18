dd50cc56128ca119314f043a3bf65dc52cee5d42
LENGLET <stephane.lenglet@gmail.com>
2019-03-18 21:00:21 +0100

    maj TestExercice3

---
+++ src/test/TestExercice3.scala
@@ -0,0 +1,47 @@
+package test
+
+import com.holdenkarau.spark.testing.{RDDComparisons, SharedSparkContext}
+import org.scalatest.FunSuite
+
+import fr.lenglet.sparktoolbox.exercices.Exercice3.{rawPersonne, rawTravel}
+
+
+class TestExercice3 extends FunSuite with SharedSparkContext {
+
+  test("Test Union rawPersonne rawTravel") {
+
+
+    val rddP = sc.textFile("src/main/resources/exercices/personne.txt")
+    val rddT = sc.textFile("src/main/resources/exercices/travel.txt")
+
+    val expected1 = "Hadoop"
+    val expected2 = "Luigi"
+
+    val resultRDD = rawPersonne(rddP)
+    val resultRDDT = rawTravel(rddT)
+
+    val rawPers = resultRDD.map(x => (x.id, (x.nom, x.prenom)))
+    val rawTrav = resultRDDT.map(x => (x.id, (x.city, x.country)))
+
+    val rawUnion = rawPers.leftOuterJoin(rawTrav)
+    println(rawUnion.count())
+
+    // Test 3.3
+    assert(rawUnion.count() == 10)
+    var rawGroup = rawUnion.groupByKey()
+    //println(rawGroup.count())
+
+    // Test 3.2
+    assert(rawGroup.count() == 5)
+
+    rawGroup.collect().foreach(x => {
+
+      if (x._1 == 4) {
+
+        var test =  x._2.toList
+        // Test 3.3
+        assert(test(0)._1._1  === expected1)
+      }
+    })
+  }
+}

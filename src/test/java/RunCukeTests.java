0297163b7181691eae39c75764fe9a999a30be71
LENGLET <stephane.lenglet@gmail.com>
2019-11-24 17:14:27 +0100

    Test cucumber report with jenkins

--- src/test/java/RunCukeTests.java
+++ src/test/java/RunCukeTests.java
@@ -4,7 +4,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/Exercice1.feature"}
+        features = {"src/test/resources/Exercice1.feature"},
+        plugin = ("json:target/cucumber-reports/CucumberTestReport.json"),
+        monochrome= true,
+        dryRun= false
 )
 class RunCukeTests {
 
